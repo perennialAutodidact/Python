fdff1eeae12dc430ab4c7408d5b393d5ca5a9a56
unknown <keegood8@gmail.com>
2020-04-01 08:57:14 -0700

    create write_css() to create a new css file and place all css within.

--- compile_scss/compile_scss_pypi/compile_scss.py
+++ compile_scss/compile_scss_pypi/compile_scss.py
@@ -1,19 +1,23 @@
 import sass
 import click
-from sys import exc_info
-from os import path, listdir
+from os import path, listdir, remove
+from shutil import copyfile
 
+def format_directory_name(directory):
+    if directory[-1] != '/':
+            directory += '/'
 
+    return directory
 
-def valid_path(filepath):
+def valid_path(file_path):
     '''
     If given file path exists, return True, 
         otherwise raise BadParameter error
     '''
-    if(path.exists(filepath)):
-        return True
+    if(path.exists(file_path)):
+        return file_path
     else:
-        raise click.BadParameter(f"The directory {filepath} does not exist.")
+        raise click.BadParameter(f"The directory {file_path} does not exist.")
 
 def dir_contains_extension(dir, ext):
     '''
@@ -24,7 +28,7 @@ def dir_contains_extension(dir, ext):
     if valid_path(dir):
         file_list = listdir(dir)
 
-    if [True for filename in file_list if '.scss' in filename] != []:
+    if [True for file_name in file_list if '.scss' in file_name] != []:
         return True
     else:
         raise click.BadParameter(f"The path: '{dir}' does not contain any files with the {ext} extension.")
@@ -41,6 +45,7 @@ def get_raw_scss(directory, scss_files):
         open it, read its contents and add them to a string of raw SCSS.
         Returns raw SCSS data
     '''
+
     raw_scss = ''
     scss_vars = ''
     for file_name in scss_files:
@@ -62,8 +67,14 @@ def get_raw_scss(directory, scss_files):
     raw_scss = scss_vars + raw_scss
     return raw_scss
 
-# def make_css(raw_scss):
+def write_css(compiled_css, css_dir, css_name):
+    new_file_path = css_dir + css_name
 
+    with open(new_file_path, 'a+') as css_file:
+        css_file.truncate(0)
+        css_file.write(compiled_css)
+
+    return
 
 # @click.option('--config', default=None, 
 #                 help='Optional path to config.txt file containing predefined set of options')
@@ -77,6 +88,10 @@ def get_raw_scss(directory, scss_files):
                 help='Format of generated CSS. Options: expanded, nested, compact, compressed')
 @click.command()
 def compile_scss(scss_dir,css_dir,css_name,output_style):
+
+    scss_dir = format_directory_name(scss_dir)
+    css_dir  = format_directory_name(css_dir)
+
     if valid_path(scss_dir):
         if dir_contains_extension(scss_dir, '.scss'):
             file_list = listdir(scss_dir)
@@ -84,4 +99,5 @@ def compile_scss(scss_dir,css_dir,css_name,output_style):
             raw_scss = get_raw_scss(scss_dir, file_list)
 
             compiled_css = sass.compile(string=raw_scss, output_style=f"{output_style}")
-            
+
+            write_css(compiled_css, css_dir, css_name)
